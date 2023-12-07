121def615943b9baddbe31468a80fb1af0771345
TearDoor <theodoretkk@gmail.com>
2023-12-07 21:07:57 +0800

    code improvements

--- srcs/operations_push.c
+++ srcs/operations_push.c
@@ -6,7 +6,16 @@
 /*   By: tkok-kea <tkok-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 17:09:36 by tkok-kea          #+#    #+#             */
-/*   Updated: 2023/12/05 17:09:37 by tkok-kea         ###   ########.fr       */
+/*   Updated: 2023/12/07 21:07:51 by tkok-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/push_swap.h"
+
+int	push(t_stack **src_stack, t_stack **dst_stack)
+{
+	if (!*src_stack)
+		return (-1);
+	(void)dst_stack;
+	return (0);
+}

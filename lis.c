90e5b50efe15eeee24fb3212144c350da5a5d640
TearDoor <theodoretkk@gmail.com>
2023-12-20 21:56:20 +0800

    code improvements

--- lis.c
+++ lis.c
@@ -1,18 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   index_stack.c                                      :+:      :+:    :+:   */
+/*   lis.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tkok-kea <tkok-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/18 21:06:25 by tkok-kea          #+#    #+#             */
-/*   Updated: 2023/12/19 21:56:48 by tkok-kea         ###   ########.fr       */
+/*   Created: 2023/12/20 16:35:52 by tkok-kea          #+#    #+#             */
+/*   Updated: 2023/12/20 16:39:04 by tkok-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/push_swap.h"
+#include "include/push_swap.h"
 
-void index_stack(t_stack **stack)
+/* function to find LIS (Largest Increasing Subsequence)
+in stack A so that we dont need to push those numbers to stack B */
+
+
+
+int main(void)
 {
-	(void)stack;
+	return (0);
 }

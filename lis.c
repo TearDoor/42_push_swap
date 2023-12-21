f2eb9b809cfbc666c4d8870497a6b17004ac2761
TearDoor <theodoretkk@gmail.com>
2023-12-21 15:45:52 +0800

    code improvements

--- lis.c
+++ lis.c
@@ -6,7 +6,7 @@
 /*   By: tkok-kea <tkok-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 16:35:52 by tkok-kea          #+#    #+#             */
-/*   Updated: 2023/12/21 15:08:20 by tkok-kea         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:45:52 by tkok-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 /* function to find LIS (Largest Increasing Subsequence)
 in stack A so that we dont need to push those numbers to stack B */
 
+int	*create_lis(int *num, int num_size, int *lis_len, int lis_max)
+{
+	int i;
+	int j;
+	int *lis;
+
+	lis = (int *)malloc(sizeof(int) * lis_max);
+	if (!lis)
+		return (NULL);
+	return (lis);
+}
 
 /* allocates an array same size as num and initialize it with 1s */
 int	*lis_len_init(int size)
@@ -76,4 +87,28 @@ int main(void)
 		printf("%d ", lis[i++]);
 
 	return (0);
+}
+
+int main()
+{
+int previous_len=0, start=0, c[10], len=0;  //c = final array; will contain the longest consecutive, increasing sequence of numbers
+int a[] = {1, 3, 5, 1, 5, 7, 8, 9, 10, 11, 12};
+for (int i = 0; i < (sizeof(a)/sizeof(int)); ++i) {
+    if(a[i+1] > a[i]) {
+        len++;
+        if (len > previous_len) { 
+            previous_len=len;
+            start=i+1-len;
+        }
+    } else {
+        previous_len=len;       
+        len=0;
+    }
+}
+for(int i = 0; i <= previous_len; ++i) {
+    c[i]=a[start+i]; //here you can copy data to output array, if you need it
+    printf("%d ",c[i]); //you can output a[start+i] instead
+}
+return 0;
+
 }

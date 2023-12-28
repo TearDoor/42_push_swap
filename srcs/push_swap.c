18a7c8916c8618e64ea2fb78ece48f91fde8549d
TearDoor <theodoretkk@gmail.com>
2023-12-28 21:08:55 +0800

    code improvements

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,27 +6,35 @@
 /*   By: tkok-kea <tkok-kea@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 18:57:59 by tkok-kea          #+#    #+#             */
-/*   Updated: 2023/12/27 19:04:57 by tkok-kea         ###   ########.fr       */
+/*   Updated: 2023/12/28 16:45:43 by tkok-kea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+static void	sort(t_stack *stack)
+{
+	t_stack *stack_pair[2];
+	int		stk_size;
+
+	stack_pair[0] = stack;
+	stack_pair[1] = NULL;
+	stk_size = ft_stack_size(stack_pair[0]);
+	if (stk_size == 2)
+		op_sa(&stack_pair[0]);
+	return ;
+}
+
 int	main(int argc, char **argv)
 {
-	t_stack	*stack_pair[2];
+	t_stack	*stack_a;
 
-	stack_pair[0] = init_stack(argc, argv);
-	stack_pair[1] = NULL;
-	if (!stack_pair[0] || stack_isdup(stack_pair[0]))
+	stack_a = init_stack(argc, argv);
+	if (!stack_a || stack_isdup(stack_a))
 		ft_error();
-	if (stack_issorted(stack_pair[0]))
-		ft_putendl_fd("Is sorted", 1);
-	ft_print_both_stacks(stack_pair);
-	ft_print_both_stacks(stack_pair);
-	op_ss(stack_pair);
-	ft_print_both_stacks(stack_pair);
-	ft_printf("%d\n", ft_stack_size(stack_pair[0]));
-	ft_stackclear(&stack_pair[0]);
+	if (stack_issorted(stack_a))
+		return (0);
+	sort(stack_a);
+	ft_stackclear(&stack_a);
 	return (0);
 }
